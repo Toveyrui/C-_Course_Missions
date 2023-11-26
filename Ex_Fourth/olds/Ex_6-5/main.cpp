12cd78e837253b49bcd56caa4c7db73d45de078c
Reiky <1149424430@qq.com>
2023-11-26 16:54:52 +0800

    补完了

--- Ex_Fourth/olds/Ex_6-5/main.cpp
+++ Ex_Fourth/olds/Ex_6-5/main.cpp
@@ -6,17 +6,31 @@
 *@see		Page 208.
 */
 #include<iostream>
+using namespace std;
 class DATE{
 	public:
 		DATE(){
-			mounth=day=year=0;
+			month=day=year=0;
 			cout<<"Default called.\n";
 		}
 		DATE(int m,int d,int y){
 			month = m;
 			day = d;
-			yearc = y;
-			cout<<"Constructor called\n"
+			year = y;
+			cout<<"Constructor called\n";
 		}
 		~DATE(){
-			cout<<
+			cout<<"Destruetor called.\n";}
+		void print(){
+			cout<<"Month="<<month<<",Day="<<day<<",Year="<<year<<endl;
+		}
+	private:
+		int month,day,year;
+};
+int main(){
+	DATE dates[5]={DATE(7,22,1998),DATE(7,23,1998),DATE(11,20,2003)};
+	dates[3]=DATE(7,25,1998);
+	dates[4]=DATE(1,7,2003);
+	for(int i(0);i<5;i++)
+		dates[i].print();
+}
